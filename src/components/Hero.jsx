401ce96636520ee1758d492107e803403da5aeb3
Oyedamola Moreira <66420203+OyedamolaM@users.noreply.github.com>
2025-09-23 06:43:34 +0100

    chore: resolve vercel hero error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,10 @@ import "./Hero.scss";
 
 export default function Hero() {
   return (
-    <section className="hero">
+        <section
+        className="hero"
+        style={{ backgroundImage: 'url(/topfrancais.jpg)' }}
+        >
       <div className="hero__content">
         <h1>Learn French with Confidence</h1>
         <p>Join the 120 million people worldwide who learn French every day. At Topfrançais, we empower you to achieve fluency in French, unlocking new opportunities and connections.</p>
